e498826f4025dae064d32804fbf6d12de09cc5b3
harish23054 <144769264+harish23054@users.noreply.github.com>
2023-09-21 12:34:35 +0530

    Add files via upload

---
+++ Day 3/src/components/pages/LandingPage.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+import '../../App.css'
+import BackgroundImage from '../../assets/images/flight.png'
+
+export default function LandingPage() {
+    return (
+        <header style={ HeaderStyle }>
+            <h1 className="main-title text-center">login / register page</h1>
+            <p className="main-para text-center">Online Flight Ticket Booking</p>
+            <div className="buttons text-center">
+                <Link to="/login">
+                    <button className="primary-button">log in</button>
+                </Link>
+                <Link to="/register">
+                    <button className="primary-button" id="reg_btn"><span>register </span></button>
+                </Link>
+            </div>
+        </header>
+    )
+}
+
+const HeaderStyle = {
+    width: "100%",
+    height: "100vh",
+    background: `url(${BackgroundImage})`,
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover"
+}
