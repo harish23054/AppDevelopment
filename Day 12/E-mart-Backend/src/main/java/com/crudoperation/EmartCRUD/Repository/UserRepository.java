da7a62098ee490e87a69a9d82fc580d00b404329
harish23054 <144769264+harish23054@users.noreply.github.com>
2023-10-05 15:28:45 +0530

    Delete Day 12/E-mart-Backend directory

--- Day 12/E-mart-Backend/src/main/java/com/crudoperation/EmartCRUD/Repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.crudoperation.EmartCRUD.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.crudoperation.EmartCRUD.Entity.UserEntity;
-
-public interface UserRepository extends JpaRepository<UserEntity, Long>{
-
-}
