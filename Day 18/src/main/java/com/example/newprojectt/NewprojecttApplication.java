14737a2f7b94f19b4b3ab408dae8f4c052865ab0
harish23054 <144769264+harish23054@users.noreply.github.com>
2023-10-11 12:19:03 +0530

    Add files via upload

---
+++ Day 18/src/main/java/com/example/newprojectt/NewprojecttApplication.java
@@ -0,0 +1,13 @@
+package com.example.newprojectt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewprojecttApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewprojecttApplication.class, args);
+	}
+
+}
---
+++ Day 18/src/main/java/com/example/newprojectt/demo/Controller.java
@@ -0,0 +1,16 @@
+package com.example.newprojectt.demo;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/user")
+public class Controller {
+    @GetMapping
+    public ResponseEntity<String> sayHello() {
+        return ResponseEntity.ok("Hello from secure endpoint");
+    }
+
+}
